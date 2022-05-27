9feb3a537676892f63f662c6feff8fca3e085ac5
Aliaxander Novikov <nerevarin11@gmail.com>
2022-05-27 13:35:25 +0300

    add ChatFieldForm, ChatFieldHeader, ChatFieldMessage

---
+++ src/App/components/Chat/components/СhatField/ChatFieldHeader/ChatFieldHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ChatFieldHeader = () => {
+  return (
+    <div className="bg-light mb-4 p-3 shadow-sm small">
+      <p className="m-0">
+        <b># random</b>
+      </p>
+      <span className="text-muted">66 сообщений</span>
+    </div>
+  )
+}
+
+export default ChatFieldHeader;
---
+++ src/App/components/Chat/components/СhatField/ChatFieldMessage/ChatFieldMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ChatFieldMessage = () => {
+  return (
+    <div className="text-break mb-2">
+      <b>абоба</b>
+      : привет
+    </div>
+  )
+}
+
+export default ChatFieldMessage;
